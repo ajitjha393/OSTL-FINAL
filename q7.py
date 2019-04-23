7d526022d810ffd7ee6a57dfc420ecd1134d7680
ajitjha393 <42679346+ajitjha393@users.noreply.github.com>
2019-04-23 21:33:35 +0530

    Update q7.py

--- q7.py
+++ q7.py
@@ -21,7 +21,7 @@ class B(Y,Z):
         print('Constructor of class B called ....')
         super().__init__()
 
-class M(A,B):
+class M(A,B,Z): #--Edited this missed Class Z to extend,(credit ---> Farhan)
     def __init__(self):
         print('Constructor of class M called ....')
         super().__init__()
